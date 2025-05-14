d6880a56fe07da36e5455e942e57739c6b4d4942
断臂残猿 <sheldon.sh.hb@gmail.com>
2025-05-14 20:29:07 +0800

    Create git-intelligence-message.rb

---
+++ Formula/git-intelligence-message.rb
@@ -0,0 +1,18 @@
+class GitIntelligenceMessage < Formula
+  desc "AI-powered Git commit message generator"
+  homepage "https://github.com/davelet/git-intelligence-message"
+  url "https://github.com/davelet/git-intelligence-message/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "d4ce8d76f9f1f15de7d67fb375224d7ddf96f9e1cd3fbcc6cacd9f4d98e8ab99"
+  license "MIT"
+  head "https://github.com/davelet/git-intelligence-message.git", branch: "main"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    system "#{bin}/gim", "--version"
+  end
+end
