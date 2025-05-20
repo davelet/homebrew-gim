94d96145de0fde92f27a7cf968e5e61fadaad0d4
断臂残猿 <sheldon.sh.hb@gmail.com>
2025-05-20 16:26:25 +0800

    Update git-intelligence-message.rb

--- Formula/git-intelligence-message.rb
+++ Formula/git-intelligence-message.rb
@@ -4,6 +4,7 @@ class GitIntelligenceMessage < Formula
   url "https://github.com/davelet/git-intelligence-message/archive/refs/tags/v1.2.0.tar.gz"
   sha256 "4beff5dc6617c7a828efc88c51ea2468b7505adfbb600206e4fac48821a2afa3"
   license "MIT"
+  version "1.2.0"
   head "https://github.com/davelet/git-intelligence-message.git", branch: "main"
 
   depends_on "rust" => :build
