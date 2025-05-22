dba6f72d9ae2b28c8d3ff96e0c793391f234ee09
断臂残猿 <sheldon.sh.hb@gmail.com>
2025-05-22 11:55:33 +0800

    Update git-intelligence-message.rb

--- Formula/git-intelligence-message.rb
+++ Formula/git-intelligence-message.rb
@@ -1,16 +1,15 @@
 class GitIntelligenceMessage < Formula
   desc "AI-powered Git commit message generator"
   homepage "https://github.com/davelet/git-intelligence-message"
-  url "https://github.com/davelet/git-intelligence-message/archive/refs/tags/v1.2.2.tar.gz"
-  sha256 "ac78bfd2c9f4e715d163e0bf341796cf5027d93fac58f00de2fb6732afa67998"
+  url "https://github.com/davelet/git-intelligence-message/archive/refs/tags/v1.3.0.tar.gz"
+  sha256 "f76ab30b21080afb93729f43060e23572930c68515c81e09c56c94d9b66b8de7"
   license "MIT"
-  version "1.2.2"
+  version "1.3.0"
   head "https://github.com/davelet/git-intelligence-message.git", branch: "main"
 
   bottle do
-    rebuild 1
-    sha256 cellar: :any_skip_relocation, sonoma: "b478f239a65b704c85cc59b1a50fcde797bbbf9c142972c08f529e5bea3f5b7a"
-    root_url "https://github.com/davelet/git-intelligence-message/releases/download/v1.2.2"
+    sha256 cellar: :any_skip_relocation, sonoma: "279306c2dd61355916935a7765a8ba88c7c6e0dcdc8c7ecb0350519cb2fb852d"
+    root_url "https://github.com/davelet/git-intelligence-message/releases/download/v1.3.0"
   end
 
   def install
