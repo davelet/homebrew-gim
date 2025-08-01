392b61615210e202d8c47033a12359d6b3e188bd
断臂残猿 <sheldon.sh.hb@gmail.com>
2025-08-01 14:41:37 +0800

    Update git-intelligence-message.rb

--- Formula/git-intelligence-message.rb
+++ Formula/git-intelligence-message.rb
@@ -1,10 +1,10 @@
 class GitIntelligenceMessage < Formula
   desc "AI-powered Git commit message generator"
   homepage "https://github.com/davelet/git-intelligence-message"
-  url "https://github.com/davelet/git-intelligence-message/archive/refs/tags/v1.5.0.tar.gz"
+  url "https://github.com/davelet/git-intelligence-message/archive/refs/tags/v1.7.0.tar.gz"
   sha256 "7032df059e5a3560b1ed98bb9da5c57142e333634bd9495a1789b5b79de92f9a"
   license "MIT"
-  version "1.5.0"
+  version "1.7.0"
   head "https://github.com/davelet/git-intelligence-message.git", branch: "main"
 
   depends_on "rust" => :build
