c8b2fa9a2d34c3cecb598736d70e8248b727f9dd
断臂残猿 <sheldon.sh.hb@gmail.com>
2025-12-17 11:34:50 +0800

    1.8.1

--- Formula/git-intelligence-message.rb
+++ Formula/git-intelligence-message.rb
@@ -1,15 +1,15 @@
 class GitIntelligenceMessage < Formula
   desc "AI-powered Git commit message generator"
   homepage "https://github.com/davelet/git-intelligence-message"
-  url "https://github.com/davelet/git-intelligence-message/archive/refs/tags/v1.8.0.tar.gz"
-  sha256 "6f2eb736b515586590f60b6cb805f87499431c2123466fa915199fea8020af1b"
+  url "https://github.com/davelet/git-intelligence-message/archive/refs/tags/v1.8.1.tar.gz"
+  sha256 "e26cba7cba44da7b1336a55da4965fd2a0d72280e00e505085baaee657fba4f3"
   license "MIT"
-  version "1.8.0"
+  version "1.8.1"
   head "https://github.com/davelet/git-intelligence-message.git", branch: "main"
 
   bottle do
-    sha256 cellar: :any_skip_relocation, sequoia: "7a8eea725d0da3aed58d3a53916a081b33e4e6d2e144d5c7404af173677f586e"
-    root_url "https://github.com/davelet/git-intelligence-message/releases/download/v1.8.0"
+    sha256 cellar: :any_skip_relocation, sequoia: "9ec1dc5c221e5f98bb7b22677feac5d31cb7c38fe9bf4736b1d704fad1ad4eaf"
+    root_url "https://github.com/davelet/git-intelligence-message/releases/download/v1.8.1"
   end
 
   def install
