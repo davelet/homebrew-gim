8729fabeea57096efe825b5a3530ab9e7cf3d61b
断臂残猿 <sheldon.sh.hb@gmail.com>
2025-12-17 10:58:44 +0800

    Update SHA256 checksum for version 1.8.1

--- Formula/git-intelligence-message.rb
+++ Formula/git-intelligence-message.rb
@@ -2,7 +2,7 @@ class GitIntelligenceMessage < Formula
   desc "AI-powered Git commit message generator"
   homepage "https://github.com/davelet/git-intelligence-message"
   url "https://github.com/davelet/git-intelligence-message/archive/refs/tags/v1.8.1.tar.gz"
-  sha256 "d6078d316b1a568374183451793d2caa952d8a9563fb1b8beae6b9f564ea41c5"
+  sha256 "e26cba7cba44da7b1336a55da4965fd2a0d72280e00e505085baaee657fba4f3"
   license "MIT"
   version "1.8.1"
   head "https://github.com/davelet/git-intelligence-message.git", branch: "main"
